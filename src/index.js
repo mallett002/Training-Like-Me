12e3538a076d1a7a9db7eec602bdc24a38dfdcd3
William Mallett <mallett002@gmail.com>
2019-07-31 19:21:43 -0500

    adding basic auth

--- src/index.js
+++ src/index.js
@@ -4,19 +4,35 @@ import {Provider} from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
+import { createFirestoreInstance } from 'redux-firestore';
 import store from './store/configure-store';
 import * as serviceWorker from './serviceWorker';
+import config from './firebase/config';
 import './index.css';
+import 'firebase/auth';
+import 'firebase/database';
+import 'firebase/storage';
+import 'firebase/firestore';
+import 'firebase/functions';
+
 
 const reduxFirebaseConfig = {
-  userProfile: 'users'
+  userProfile: 'users',
+  useFirestoreForProfile: true 
 };
 
 const rrfProps = {
     firebase,
     config: reduxFirebaseConfig,
-    dispatch: store.dispatch
+    dispatch: store.dispatch,
+    createFirestoreInstance
 }
+
+console.log({config});
+
+firebase.initializeApp(config);
+firebase.firestore();
+firebase.functions();
 
 const AppWithStore = () => (
   <Provider store={store}>
--- src/store/configure-store-dev.js
+++ src/store/configure-store-dev.js
@@ -1,11 +1,7 @@
 import { applyMiddleware, createStore } from 'redux';
-import config from '../firebase/config';
-import firebase from 'firebase/app';
 import logger from 'redux-logger';
 import rootReducer from '../root-reducer';
 import 'firebase/auth';
-
-firebase.initializeApp(config);
   
 const initialState = {};
 
